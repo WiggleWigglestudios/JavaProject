4ae81a9910407d120734f675ab11682cd4152d78
WiggleWigglestudios <55055255+WiggleWigglestudios@users.noreply.github.com>
2021-11-02 10:42:02 -0600

    0.0

---
+++ VideoGameTextBase/src/TextTrigger.java
@@ -0,0 +1,14 @@
+
+public class TextTrigger {
+	
+	String text="";
+	int pos[]= {};
+	TextTrigger(String _text,int _xLocation,int _yLocation){
+		text=_text;
+		pos[0]=_xLocation;
+		pos[1]=_yLocation;
+		
+	};
+	
+
+}
---
+++ VideoGameTextBase/src/main.java
@@ -0,0 +1,27 @@
+
+public class main {
+	
+	int playerPos[]= {1,1};
+
+	int levelOn=0;
+	
+	
+	//1 is block 0 air 2 is door 3 is key 4 is enemy  
+	int levels[][][]= {
+			{
+				{1,1,1},
+				{1,0,1},
+				{1,3,1},
+				{1,2,1},
+				{1,1,1},
+				
+			}
+	};
+	TextTrigger tx=new TextTrigger("a",1,1);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("a");
+	}
+
+}
